2fca5e037e06d66972eba9b9472ed06a34d47974
vividmuimui <1803598+vividmuimui@users.noreply.github.com>
2021-12-22 18:59:55 +0900

    Reference resource, add posts

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import * as React from "react";
 import { Admin, Resource } from 'react-admin';
 import jsonServerProvider from "ra-data-json-server";
 import { UserList } from "./users";
+import { PostList } from "./posts";
 
 const dataProvider = jsonServerProvider("https://jsonplaceholder.typicode.com")
 const App = () => (
   <Admin dataProvider={dataProvider}>
+    <Resource name="posts" list={PostList} />
     <Resource name="users" list={UserList} />
   </Admin>
 )
